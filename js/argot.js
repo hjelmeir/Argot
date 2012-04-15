a67b94bb874c37123321049a4098bd73b53ac9f6
Robin Anderson <robin@ipixelsmediaworks.com>
2012-04-15 13:23:13 -0400

    initial commit

---
+++ js/argot.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function ($) {
+  $('.navbar-fixed-top').hover(
+    function () {
+    var $navbar = $(this);
+    $navbar.stop().animate({ top: 0 });
+  },
+  function() {
+    var $navbar = $(this);
+    $navbar.stop().animate({ top: '-32px' });
+  });
+  $('.tabs li a').click(function(e) {
+    e.preventDefault();
+    var currentTab = $(this).closest('li');
+    var tabs = $('.tabs');
+    tabs.find('.active')
+      .stop()
+      .animate({ top: 0 }, 'fast',
+              function() {
+                $(this).removeClass('active');
+                currentTab.animate({ top: '3em' }).addClass('active');
+              });
+    //currentTab.animate({ top: '3em' }).addClass('active');
+  });
+});
